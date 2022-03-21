5bac1de0a576a4336dd5ad9bd7bf408a094a3335
VellaiT93 <tibivellai@gmail.com>
2022-03-21 13:45:36 +0000

    Add files via upload

---
+++ theme_apply_sp19.ps1
@@ -0,0 +1,21 @@
+﻿Add-PSSnapin Microsoft.SharePoint.Powershell -ErrorAction SilentlyContinue
+ 
+#Variables for processing
+$WebURL = "http://pwwpk-pnet-ch/sites/home/beruflichevorsorge"
+
+#Get the Web
+$web = Get-SPWeb -Identity $WebURL
+
+$folder = $web.GetFolder("_catalogs/theme/15")
+
+$ColorPaletteURL = "/sites/home/_catalogs/theme/15/Palette_pkpost.spcolor"
+$FontSchemeURL = "/sites/home/_catalogs/theme/15/fontscheme005.spfont"
+$BackgroundImgURL= "/sites/home/_layouts/15/images/image_bg003.jpg"
+
+#Apply theme
+$web.ApplyTheme($ColorPaletteURL, $FontSchemeURL, "", $true)
+
+$web.Update()
+$web.dispose()
+
+Write-Host "Script terminated succesfully"
